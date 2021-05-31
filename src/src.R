18f7d425f546f2b4e89dea9ff3d6cfd2945329cf
Elli van Berlekom <erik.vanberlekom@psychology.su.se>
2021-05-31 15:35:53 +0200

    edits to source and markdown scripts

---
+++ src/src.R
@@ -0,0 +1,30 @@
+manipulation_check <- function(d){
+  #manipulation check
+  conditions <- c("hen1", "hen2", "hen3", "han1", "han2", "han3", "hon1", "hon2", "hon3", "control1", "control2", "control3")
+  
+  #Manipulation check make sure participants actually used the pronoun in all
+  #three sentences. I will admit, this is all pretty hackish. 
+  
+  #First, create a column called condition in d with the first three letters in each of
+  #the three sentences
+  d_conditions <- apply( d[,conditions], 2 , substr, start = 1, stop = 3 ) 
+  d_conditions <-  t(apply(d_conditions, 1, function(x) c(x[!is.na(x)], x[is.na(x)]))) %>%
+    data.frame() %>% select(1:3)
+  d$condition_tmp <- paste(d_conditions[,1], d_conditions[,2], d_conditions[,3], sep = "") %>%
+    tolower()
+  
+  #then check if they used the same pronoun in the all three sentencs AND if they weren't
+  #in the control condition
+  d$completed <- d$condition_tmp == "henhenhen"|d$condition_tmp == "hanhanhan"| d$condition_tmp == "honhonhon"
+  doublecheck <- function(x){
+    ifelse(d$completed == FALSE, grepl(x, d$condition_tmp), F) 
+  }
+  doublechecks <- doublecheck("han") == T & doublecheck("hon") == T & doublecheck("hen") == T
+  d$condition <- ifelse(d$completed == F, "control", d$condition_tmp) %>% 
+    ifelse(doublechecks == T, NA, .) %>% 
+    recode(., "henhenhen" = "hen", "honhonhon" = "hon", "hanhanhan" = "han")
+  
+  for (i in c("hen", "na", "han", "hon")){
+    d$condition <- ifelse(d$completed == F & grepl(i, d$condition_tmp) == T, NA, d$condition ) 
+  }
+}
